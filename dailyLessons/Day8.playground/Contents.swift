b7a392badef567daf1510535cace591bea163aee
Ari-Juan <172550929+Ari-Juan@users.noreply.github.com>
2025-09-30 17:50:35 -0500

    Day 8 addition

--- dailyLessons/Day8.playground/Contents.swift
+++ dailyLessons/Day8.playground/Contents.swift
@@ -1,3 +1,29 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// assign defualt values for our paramenters
+
+func printTimeTables(for number: Int, end: Int = 8){
+    for i in 1...end{
+        print("\(i) x \(number) is \( i * number)")
+    }
+}
+
+printTimeTables(for: 5, end: 12)
+printTimeTables(for: 3,)
+
+//triggering errors in functions
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String{
+    if password.count < 5 {
+        throw PasswordError.shot
+    }
+    
+    if password == "12345" {
+        
+    }
+    
+}
