15341640f04ee566513e7d6c74e9f9083fabd307
Ari-Juan <172550929+Ari-Juan@users.noreply.github.com>
2025-10-01 15:40:43 -0500

    Day 8-9 pt2

--- dailyLessons/Day8.playground/Contents.swift
+++ dailyLessons/Day8.playground/Contents.swift
@@ -19,11 +19,28 @@ enum PasswordError: Error {
 
 func checkPassword(_ password: String) throws -> String{
     if password.count < 5 {
-        throw PasswordError.shot
+        throw PasswordError.short
     }
-    
     if password == "12345" {
-        
+        throw PasswordError.obvious
     }
-    
+    if password.count < 8{
+        return "Ok"
+    }
+    else if password.count < 10 {
+        return "Good"
+    }
+    else{
+        return "Very good"
+    }
+}
+
+let string = "12345"
+
+do{
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+}
+catch{
+    print("There was an error.")
 }
